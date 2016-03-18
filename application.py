49f113ed32176f30fce68ddbcb9f516f2873e4f0
Kent Anthony Astudillo <bone@Kents-MacBook-Pro.local>
2016-03-18 11:17:05 +0800

    remove test.py

--- application.py
+++ application.py
@@ -6,20 +6,7 @@ application.debug = True
 
 @application.route('/')
 def hello_world():
-  storage = Storage()
-  storage.populate()
-  score = storage.score()
-  return "Hello world, %d!" % score
-
-class Storage():
-  def __init__(self):
-    self.db = dict()
-
-  def populate(self):
-    self.db['score'] = 1234
-
-  def score(self):
-    return self.db['score']
+    print 'oh no!'
 
 if __name__ == "__main__":
   application.run(host='0.0.0.0')
